d7159b2fc628242186d01ccfd1dfa693812f7990
taneluprus <166364410+taneluprus@users.noreply.github.com>
2024-05-29 21:56:14 +0300

    Add files via upload

---
+++ webscraper.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import openpyxl
+import datetime
+import time
+#requierments - bs4, requests, openpyxl, datetime, time
+#file requierments (current) - startcolrecord.txt, testexcel1.xlsx
+#[TODO] CHANGE FILENAMES at some point
+
+teststartcol=1
+
+while True:
+    url='https://hipikuut.com/weewx/'
+    page=requests.get(url)
+    soup=BeautifulSoup(page.text, 'html.parser')
+
+    #print(soup.find('table'))
+    teststr=str(soup.find('table'))
+
+    removelist=['<table>', '<tbody>', '<tr>', '</tr>', '</tbody>', '</table>', '</td>', '<td class="label">','<td class="data">', '°', '\n\n']
+    for word in removelist:
+        teststr=teststr.replace(word, '')
+
+    teststr=teststr.replace('²','2')
+    teststr=teststr.strip()
+
+    andmed={}
+
+    andmedlines = teststr.splitlines()
+
+    def convert(lst):
+       res_dict = {}
+       for i in range(0, len(lst), 2):
+           res_dict[lst[i]] = lst[i + 1]
+       return res_dict
+
+    andmed=convert(andmedlines)
+
+    #print(andmed)
+
+    #f=open('C:\\Users\\Tanel\\Desktop\\testfile.txt', 'w')
+    #f.write(str(andmed))
+    #f.close()
+    andmedvalues=andmed.values()
+
+    currenttime=datetime.datetime.now().strftime('%H:%M')
+    #print(time)
+    timelist=[]
+    timelist.append(currenttime)
+
+    df=pd.DataFrame(andmedvalues, columns=timelist)
+    #df=(df.T)
+    #print(df)
+
+    with pd.ExcelWriter('C:\\Users\\Tanel\\Desktop\\testexcel1.xlsx', mode='a',engine='openpyxl', if_sheet_exists='overlay') as writer:
+        df.to_excel(writer, sheet_name='Sheet1', startcol=teststartcol, index=False)
+        teststartcol+=1
+    
+    f1=open(r'C:\Users\Tanel\Desktop\startcolrecord.txt', 'a')
+    f1.write(str(teststartcol))
+    f1.write('\n')  
+    f1.close()
+
+    time.sleep(60)
+    
